3f5f85bbfd3fa600b39ec11df5984d733462db51
Robyn Trovati <robyntrovati@mac.com>
2017-04-02 16:01:12 -0400

    Experiments

---
+++ javascripts/candy.js
@@ -0,0 +1,51 @@
+var vis1 = {
+  container: '#lissajous1',
+  width: 200,
+  height: 200,
+  strokeColor: '#efddc8',
+  fillColor: '#e9f5dc',
+  strokeWidth: 0.5,
+  maxCurves: 9,
+  curveType: curveTypes.perquex,
+  curved: true,
+  incrementCurves: false,
+}
+
+var vis3 = {
+  container: '#lissajous3',
+  width: 200,
+  height: 200,
+  strokeColor: '#dcdcf5',
+  fillColor: '#dcf5f5',
+  strokeWidth: 0.5,
+  maxCurves: 8,
+  curveType: curveTypes.persort,
+  curved: true,
+  incrementCurves: false,
+}
+
+var vis2 = {
+  container: '#lissajous2',
+  width: 200,
+  height: 200,
+  strokeColor: '#dcdcf5',
+  fillColor: '#f5efdc',
+  strokeWidth: 0.5,
+  maxCurves: 9,
+  curveType: curveTypes.perming,
+  curved: true,
+  incrementCurves: false,
+}
+
+var vis4 = {
+  container: '#lissajous4',
+  width: 200,
+  height: 200,
+  strokeColor: '#dcf5dc',
+  fillColor: '#f5dcdc',
+  strokeWidth: 0.5,
+  maxCurves: 9,
+  curveType: curveTypes.perlume,
+  curved: true,
+  incrementCurves: false,
+}
